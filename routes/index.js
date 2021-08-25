88a0225a88916346fdd2586b03d466bbe5901470
BoboLin <bobolincowo@gmail.com>
2021-08-25 16:44:37 +0800

    feat: add admin restaurant create view and feature

--- routes/index.js
+++ routes/index.js
@@ -22,11 +22,12 @@ module.exports = (app, passport) => {
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', authenticated, restController.getRestaurants)
 
-  // 連到 /admin 頁面就轉到 /admin/restaurants
+  // 後台首頁
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
-
-  // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
+  // 後台餐廳列表 (CRUD)
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
+  app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
 
   // 註冊
   app.get('/signup', userController.signUpPage)
