32c552132389f48369f21231b7efcb6292043f7c
BoboLin <bobolincowo@gmail.com>
2021-08-30 10:58:02 +0800

    feat: add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,21 +2,36 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const pageLimit = 10 // 一頁 10 筆資料
+
 const restController = {
   // 前台 取得餐廳列表
   getRestaurants: (req, res) => {
+    let offset = 0
     const whereQuery = {}
     let categoryId = ''
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery.CategoryId = categoryId
     }
-    Restaurant.findAll({
+    Restaurant.findAndCountAll({
       include: Category,
-      where: whereQuery
+      where: whereQuery,
+      offset,
+      limit: pageLimit
     })
-      .then((restaurants) => {
-        const data = restaurants.map(r => ({
+      .then((result) => {
+        const page = Number(req.query.page) || 1
+        // 計算共有幾頁
+        const pages = Math.ceil(result.count / pageLimit)
+        // 頁數陣列
+        const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+        const prev = page - 1 < 1 ? 1 : page - 1
+        const next = page + 1 > pages ? page : page + 1
+        const data = result.rows.map(r => ({
           ...r.dataValues,
           description: r.dataValues.description.substring(0, 50),
           categoryName: r.Category.name
@@ -26,7 +41,7 @@ const restController = {
           nest: true
         })
           .then((categories) => {
-            return res.render('restaurants', { restaurants: data, categories, categoryId })
+            return res.render('restaurants', { restaurants: data, categories, categoryId, page, totalPage, prev, next })
           })
       })
   },
