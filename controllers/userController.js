84cac3ee27a785df9000f49f89c81cc6cdcdf3c7
BoboLin <bobolincowo@gmail.com>
2021-09-02 22:34:09 +0800

    modify: user profile page and getUser feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,36 +68,49 @@ const userController = {
   getUser: (req, res) => {
     const id = req.params.id
     const loginUserId = helpers.getUser(req).id
-    // 找到有此 user 的評論
-    Comment.findAndCountAll({
-      include: Restaurant,
-      where: { UserId: id }
+    User.findByPk(id, {
+      include: [
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' },
+        { model: Comment, include: [Restaurant] },
+        { model: Restaurant, as: 'FavoritedRestaurants' }
+      ]
+    }).then((user) => {
+      // 是否已追蹤此 user
+      const isFollowed = user.Followers.map((d) => d.id).includes(loginUserId)
+      // 收藏的餐廳
+      let restaurants = user.dataValues.FavoritedRestaurants
+      restaurants = user.dataValues.FavoritedRestaurants.map(restaurant => ({
+        ...restaurant.dataValues,
+      }))
+      const resNum = Number(restaurants.length)
+      // 評論過餐廳
+      let comments = user.dataValues.Comments
+      comments = comments.map(comment => ({
+        ...comment.dataValues,
+        commentedRestaurants: comment.Restaurant.dataValues
+      }))
+      let result = []
+      comments.forEach(comment => result.push(comment.commentedRestaurants))
+      // 剔除重複餐廳
+      const set = new Set()
+      const commentedRes = result.filter(res => !set.has(res.id) ? set.add(res.id) : false)
+      const comNum = Number(comments.length)
+      // Followers
+      let followers = user.dataValues.Followers
+      followers = followers.map(follower => ({
+        ...follower.dataValues,
+      }))
+      const followerNum = Number(followers.length)
+      // Followings
+      let followings = user.dataValues.Followings
+      followings = followings.map(following => ({
+        ...following.dataValues,
+      }))
+      const followingNum = Number(followings.length)
+
+      res.render('user', { userProfile: user.toJSON(), loginUserId, resNum, comNum, followerNum, followingNum, restaurants, isFollowed, followers, followings, commentedRes })
     })
-      .then((result) => {
-        const commentNum = result.count
-        let query = []
-        new Promise((resolve, reject) => {
-          const data = result.rows.map(r => ({
-            ...r.dataValues
-          }))
-          for (let i = 0; i < data.length; i++) { query.push(data[i].RestaurantId) }
-          resolve(query)
-        }).then((query) => {
-          Restaurant.findAll({
-            raw: true,
-            nest: true,
-            where: { id: query }
-          })
-            .then((restaurants) => {
-              User.findByPk(id)
-                .then((user) => {
-                  const userProfile = user.toJSON()
-                  res.render('user', { userProfile, loginUserId, commentNum, restaurants })
-                })
-                .catch(err => console.log(err))
-            })
-        })
-      })
   },
   // edit profile page
   editUser: (req, res) => {
@@ -230,7 +243,7 @@ const userController = {
     const followerId = helpers.getUser(req).id
     const followingId = req.params.userId
     Followship.findOne({
-      followerId, followingId
+      where: { followerId, followingId }
     }).then((followship) => {
       followship.destroy()
     }).then((followship) => {
